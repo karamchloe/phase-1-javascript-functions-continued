7aa245527c1d72f51631f28610f125a27f8e2191
Chloe Park <kpark0119@gmail.com>
2023-10-03 01:15:48 -0400

    IIFE

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+        return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun("I want to bathe my dog!")
+
+function mondayWork(activity = "go to the office"){
+   return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home.")
+
+
+function wrapAdjective(asterisk){
+    return function innerFunc(adjective ="special"){
+        return `You are ${asterisk}${adjective}${asterisk}!`
+    }
+}
+
+let highlight = wrapAdjective("*");
+let fullSentence = highlight("a hard worker")
+
+let emphasize = wrapAdjective("||")
+let newSentence = emphasize("a dedicated programmer")
